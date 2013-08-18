e27a28e49dcbcd68220b8f97e4e009ec6c639246
MardamBey <iam.mardambey@gmail.com>
2013-08-18 18:40:45 +0200

    Add a "filter by roles" feature
    Very practical when your wordpress is a real website, and not a simple
    blog.

--- js/restrict-categories.js
+++ js/restrict-categories.js
@@ -11,5 +11,33 @@ jQuery(document).ready(function($) {
 		else
 			items.prop( 'checked', true );
 	});
-	
+
+	// Filter feature
+	$("input[id^='toggleBoxes-']").click( function( e ) {
+
+		var value = $(this).val();
+
+		if(value == 'all') {
+			// Check all the checkboxes
+			if($(this).is(":checked")) {
+				$("input[id^='toggleBoxes-']").attr('checked', true);
+				$("div[class^='userlevel-']").show();
+			} 
+			// Uncheck all the checkboxes
+			else {
+				$("input[id^='toggleBoxes-']").attr('checked', false);
+				$("div[class^='userlevel-']").hide();
+			}
+		} else {
+			// Show selected box(es)
+			if($(this).is(":checked")) {
+				$('.userlevel-'+value).show();
+			} 
+			// Hide selected box(es)
+			else {
+				$('.userlevel-'+value).hide();
+				$(':input[value="all"]').attr('checked', false);
+			}
+		}
+	});
 });
